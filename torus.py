e9c7a96c1bee68d7d33714025a51639a0b00f15e
Soham De <sohamde@umiacs.umd.edu>
2015-09-24 19:50:08 -0400

    cleaning up code

--- torus.py
+++ torus.py
@@ -2,14 +2,18 @@
 #
 # Torus class to facilitate a grid environment on which agents interact.
 #
-# @author: roos@cs.umd.edu	
+# @author: Soham De, Patrick Roos
 # #############################
+
 
 class Torus:
 	"""
 	Torus class to facilitate a grid environment on which agents interact.
 	"""
 	def __init__(self, nrows, ncols, neighborhood, reproductionneighborhood):
+		"""
+		Creates a grid torus specifying grid neighborhoods and reproduction neighborhoods for each location
+		"""
 		self.nrows, self.ncols = nrows, ncols
 		self.neighborhood = neighborhood # list of tuples of x,y-offsets (relative to any grid location).
 		self.reproductionneighborhood = reproductionneighborhood # list of tuples of x,y-offsets (relative to any grid location).
@@ -17,7 +21,8 @@ class Torus:
 		
 		self.emptySites = [(i,j) for i in range(nrows) for j in range(ncols)]
 
-		self.neighborLocs = {} # for each location (x,y) key will hold neighboring locations as list
+		# for each location (x,y) key will hold neighboring locations as list
+		self.neighborLocs = {}
 		for i in range(self.nrows):
 			for j in range(self.ncols):
 				neighLocs = list()
@@ -28,6 +33,7 @@ class Torus:
 					neighLocs.append((r,c))
 				self.neighborLocs[(i,j)] = neighLocs
 
+		# for each location (x,y) key will hold reproduction neighborhoods as list
 		self.reproductionneighborLocs = {}
 		for i in range(self.nrows):
 			for j in range(self.ncols):
@@ -39,12 +45,8 @@ class Torus:
 					neighLocs.append((r,c))
 				self.reproductionneighborLocs[(i,j)] = neighLocs
 
-		#print "neighLocs.keys():", self.neighborLocs.keys()
-
 	def place_agent(self, agent, (x,y)):
-		"""
-		Places agent on grid at (x,y).
-		"""
+		""" Places agent on grid at (x,y). """
 		agent.gridlocation = (x,y)
 		self.agentMatrix[x][y] = agent
 		self.emptySites.remove((x,y))
@@ -74,7 +76,6 @@ class Torus:
 	
 	def get_all_neigh_agent_pairs(self):
 		""" Returns list of all pairs of neighboring locations that have agents in them. """
-	
 		pairs = set()
 		for origLoc in self.neighborLocs.keys():
 			if self.agentMatrix[origLoc[0]][origLoc[1]] != None: # there is an agent here
@@ -83,12 +84,10 @@ class Torus:
 						if (self.agentMatrix[origLoc[0]][origLoc[1]],self.agentMatrix[loc[0]][loc[1]]) not in pairs and \
 							(self.agentMatrix[loc[0]][loc[1]],self.agentMatrix[origLoc[0]][origLoc[1]]) not in pairs:
 							pairs.add((self.agentMatrix[origLoc[0]][origLoc[1]],self.agentMatrix[loc[0]][loc[1]]))
-
 		return list(pairs)
 
 	def get_all_triplets(self):
 		"""returns all list of triplets"""
-
 		triplets = list()
 		for origLoc in self.neighborLocs.keys():
 			if self.agentMatrix[origLoc[0]][origLoc[1]] != None: # there is an agent here
@@ -100,90 +99,43 @@ class Torus:
 						common_neighbors = [neigh for neigh in neighbors_i if neigh in neighbors_j]
 						if len(common_neighbors) == 2:
 							triplets.append([self.agentMatrix[origLoc[0]][origLoc[1]], neighbors_agent[i], neighbors_agent[j]])
-
 		return triplets
 
-
 	def get_empty_sites(self):
-		"""
-		Returns list of (x,y) tuples that are empty grid locations.
-		"""
+		""" Returns list of (x,y) tuples that are empty grid locations. """
 		return self.emptySites
-	
 
 	def get_empty_locs_in_range(self,location, r):
+		""" Return empty location within range r of location """
 		locs_in_range = []
 		for i in range(location[0] - r, location[0] + r):
 			for j in range(location[1] - r, location[1] + r):
 				locs_in_range.append( ( i%self.nrows, j%self.ncols) )
-
 		return [loc for loc in locs_in_range if self.agentMatrix[loc[0]][loc[1]] == None]
-	
 
 	def get_neighbors(self, agent):
-		"""
-		Return neighbors of agent.
-		"""
-
-		#agent_row, agent_col = agent.gridlocation
+		""" Return neighbors of agent. """
 		neighbors = list()
 		neighlocs = self.neighborLocs[agent.gridlocation]
 		for loc in neighlocs:
 			if self.agentMatrix[loc[0]][loc[1]] != None:
 				neighbors.append(self.agentMatrix[loc[0]][loc[1]])
-
-		#nrows, ncols = grid.nrows, grid.ncols
-		#agents2d = grid.agents2d
-
-		# initialize list of neighbors
-		#neighbors = list()
-
-		# append all neighbors to list
-		#for offset in self.neighborhood:
-		#	dc, dr = offset		 #note: x,y neighborhood
-		#	r = (agent_row + dr) % nrows
-		#	c = (agent_col + dc) % ncols
-		#	neighbor = agents2d[r][c]
-		#	neighbors.append(neighbor)
-
 		return neighbors
 
-
 	def get_neighbors_of_loc(self, loc):
-		"""
-		Return neighbors of agent.
-		"""
-
-		#agent_row, agent_col = agent.gridlocation
+		""" Return neighbors of agent. """
 		neighbors = list()
 		neighlocs = self.neighborLocs[loc]
 		for loc in neighlocs:
 			if self.agentMatrix[loc[0]][loc[1]] != None:
 				neighbors.append(self.agentMatrix[loc[0]][loc[1]])
-
-		#nrows, ncols = grid.nrows, grid.ncols
-		#agents2d = grid.agents2d
-
-		# initialize list of neighbors
-		#neighbors = list()
-
-		# append all neighbors to list
-		#for offset in self.neighborhood:
-		#	dc, dr = offset		 #note: x,y neighborhood
-		#	r = (agent_row + dr) % nrows
-		#	c = (agent_col + dc) % ncols
-		#	neighbor = agents2d[r][c]
-		#	neighbors.append(neighbor)
-
 		return neighbors
-
 
 	def place_all(self, agentsList):
 		"""
 		Fills grid with agents, in sequential order at (0,0), (0,1) ... (n,n).
 		Assumes len(agents) is enough for every grid location.
 		"""
-
 		agents = iter(agentsList)
 		# put a agent in each grid location (row, column)
 		for row in range(self.nrows):
@@ -192,5 +144,3 @@ class Torus:
 				self.agentMatrix[row][column] = agent
 				agent.gridlocation = (row, column)
 				self.emptySites.remove((row, column))
-				
-
