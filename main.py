4fcc0a1fefc939d866d116d025c8924baae30439
Soham De <sohamde@umiacs.umd.edu>
2015-09-25 01:14:21 -0400

    cleaning up code

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-# ###############################
-# main.py
-#
-# *** Group-Entitativity vs Individual-Entitativity simulator. ***
-#
-# USAGE:	
-# - use main() to run simulation
-# - or pass init(), draw(), and step() to the pycx simulator GUI and run the simulation in GUI
-#
-# @author: Soham De, Patrick Roos
-# @version: sep-2015
+## Group-Entitativity & Individual-Entitativity vs Mobility simulator
+##
+## USAGE: python main.py run_no mobility_probability
+## where run_no is an integer and mobility_probability is a float between 0 and 1
+##
 ## authors: soham de, patrick roos
 ## emails: (sohamde, roos) at cs umd edu
 
@@ -17,20 +11,27 @@ from phases import *
 
 def step(agents, grid, counts):
 	"""
-	Steps through time period stages by Hammond and Axelrod (2006):
-	- immigration, interaction, reproduction, death, mobility
+	Steps through evolutionary phases using methods in phases.py
+	Follows same sequence of phases as in Hammond & Axelrod 2006: immigration, interaction, reproduction, death
+	Mobility phase is added after death phase
 	"""
 
+	# immigration phase
 	agents, grid = immigration(agents, grid)
 
+	# interaction phase
 	agents, grid, pairs, numInteractions, inCoops, inDefects, outCoops, outDefects, totalOutgroupInteractions = interaction(agents, grid)
 
+	# reproduction phase
 	agents, grid, counts = reproduction(agents, grid, counts)
 
+	# death phase
 	agents, grid = death(agents, grid)
 
+	# mobility phase
 	agents, grid = mobility(agents, grid)
 
+	# calculate statistics for result files: percentage of interactions with out-group agents
 	totalOutgroupInteractionPerc = 0
 	if g.PAIRALLNEIGHS == True:
 		if len(pairs) > 0:
@@ -39,28 +40,35 @@ def step(agents, grid, counts):
 		if numInteractions > 0:
 			totalOutgroupInteractionPerc = totalOutgroupInteractions/float(numInteractions)
 
+	# calculate statistics for result files: percentage of agents alive, clustering coefficient, etc
 	alive_proportion = float((grid.nrows*grid.ncols) - len(grid.emptySites))/float(grid.nrows*grid.ncols)
-
 	clustering_coeff = empty_ignore(agents,grid)
-
 	diff_games_list = [g.avg_diff_agents, g.avg_same, g.avg_same_gt_1]
 
+	# calculating statistics and writing to output result files
 	counts = g.stats.step(agents, inCoops, inDefects, outCoops, outDefects, totalOutgroupInteractionPerc, alive_proportion, clustering_coeff, diff_games_list)
 
 	return agents, grid, counts
 
 
-########################################################################################################
 if __name__ == "__main__":
 
 	try:
 		time = 0
+		# initialize grid
 		agents_list, grid_object, stat_counts = init(g.grid_initialization)
 		print "initialized."
+
+		# for each iteration
 		while time < g.maxTime:
+			# run different evolutionary phases
 			agents_list, grid_object, stat_counts = step(agents_list, grid_object, stat_counts)
 			if time%10 == 0:
 				print "time:", time
+
+			# update iteration count
 			time += 1
+
 	finally:
+		# close output result files
 		g.stats.close_files()
