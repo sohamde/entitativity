354e79d20bf8c17324b67ded6fe4840e20737591
Soham De <sohamde@umiacs.umd.edu>
2015-09-24 22:40:12 -0400

    cleaning up code

--- stats_files.py
+++ stats_files.py
@@ -1,18 +1,20 @@
-# ##########################
-#
-#  *** To keep track of population statistics. ****
-#   
-#
-# @author: roos@cs.umd.edu
-# @version: aug-2013
-# ##########################
+## Keep track of population statistics
+## authors: soham de, patrick roos
+## emails: (sohamde, roos) at cs umd edu
 
 
 class Stats:
+	"""
+	Class to keep track of population statistics and output the results into different files
+	"""
 
 	def __init__(self, tags, runID="x", results_folder='./results/'):
+		"""
+		initialize output files and variables to store statistics
+		"""
 
 		self.tags = tags
+
 		# will hold the latest (current) val of all the following, so that we can plot it:
 		self.inGrpDefPerc = 0
 		self.outGrpDefPerc = 0
@@ -24,6 +26,7 @@ class Stats:
 		self.avgInP_curr = self.avgInQ_curr = self.avgInI_curr = 0	# collectivist's in-group pqi averages
 		self.avgOutP_curr = self.avgOutQ_curr = self.avgOutI_curr = 0 # collectivist's out-group pqi averages
 
+		# 'results' file contains statistics such as percentage of cooperation, defection, in-group and out-group cooperation, etc
 		self.resultFile = open(results_folder+"results_"+str(runID)+".txt",'wb')
 		self.resultFile.write("inGrpDefPerc,outGrpDefPerc,coopProp,outGrpIntProp,collProp,indProp,avgP,avgQ,avgI,avgInP,avgInQ,avgInI,avgOutP,avgOutQ,avgOutI\n")		
 		self.tagCountFile = open(results_folder+"tagCounts_"+str(runID)+".txt",'wb')
@@ -33,8 +36,11 @@ class Stats:
 			else:
 				self.tagCountFile.write(str(tag)+",")
 
+		# types of strategies used; first bit: action if in-group/out-group/individual cooperated on last move
+		# second bit: action if in-group/out-group/individual defected on last move; 0: cooperate, 1: defect
 		self.stratTypes = ["00","01","10","11"]
 
+		# 'indivStratCounts' file contains percentage of 'stratTypes' for individual-entitative agents
 		self.indivStratCountFile = open(results_folder+"indivStratCounts_"+str(runID)+".txt",'wb')
 		for strat in self.stratTypes:
 			if strat == self.stratTypes[-1]:
@@ -42,6 +48,7 @@ class Stats:
 			else:
 				self.indivStratCountFile.write(strat+",")
 
+		# 'inStratCounts' file contains percentage of 'stratTypes' for group-entitative agents against in-groups
 		self.inStratCountFile = open(results_folder+"inStratCounts_"+str(runID)+".txt",'wb')
 		for strat in self.stratTypes:
 			if strat == self.stratTypes[-1]:
@@ -49,6 +56,7 @@ class Stats:
 			else:
 				self.inStratCountFile.write(strat+",")
 
+		# 'inStratCounts' file contains percentage of 'stratTypes' for group-entitative agents against out-groups
 		self.outStratCountFile = open(results_folder+"outStratCounts_"+str(runID)+".txt",'wb')
 		for strat in self.stratTypes:
 			if strat == self.stratTypes[-1]:
@@ -56,16 +64,18 @@ class Stats:
 			else:
 				self.outStratCountFile.write(strat+",")
 
+		# 'alive' file contains percentage of nodes in the grid populated by agents
 		self.alive_file = open(results_folder+"alive_"+str(runID)+".txt","wb")
 		self.alive_file.write("alive_proportion\n")
 
+		# 'coeff' file contains value of the modified clustering coefficient in the graph
 		self.coeff_file = open(results_folder+"coeff_"+str(runID)+".txt",'wb')
 		self.coeff_file.write("clustering_coefficient\n")
 
+		# 'diff_games' contains the average number of unique opponents each agent encounters, and the average number of games played against each of them
 		self.diff_games_file = open(results_folder+"diff_games_"+str(runID)+".txt","wb")
 		self.diff_games_file.write("no_unique_games,no_games_same_agent,no_games_same_agent_gt_1\n")
 
-	
 	def step(self, agents, inCoops, inDefects, outCoops, outDefects, outGroupIntPerc, alive_proportion, clustering_coeff, diff_games_list):
 		""" Records everything for this time step. """
 			
@@ -79,7 +89,7 @@ class Stats:
 			else:
 				self.tagCountFile.write(str(counts[tag])+",") 
 
-		# split agents by Coll vs Ind trait
+		# split agents by group-entitative or individual-entitative trait
 		collAgents = []
 		indAgents = []
 		for a in agents:
@@ -88,7 +98,7 @@ class Stats:
 			else:
 				indAgents.append(a)
 
-		# calc col and ind pop proportion
+		# calculate group-entitative and individual-entitative population proportion
 		nColl = len(collAgents)
 		nInd = len(indAgents)
 		if n > 0:
@@ -97,7 +107,7 @@ class Stats:
 		else:
 			collProp = indProp = 0
 			
-		# get in out and ind pqi
+		# get strategy proportions for individual-entitative, group-entitative in-group and out-group agents
 		pCount, qCount, iCount = self.getInPandQCounts(collAgents)
 		if nColl > 0:
 			avgInP = pCount/float(nColl)
@@ -153,10 +163,9 @@ class Stats:
 		resultstr = str(self.inGrpDefPerc)+","+str(self.outGrpDefPerc)+","+str(cProp)+","+str(outGroupIntPerc)+","+str(collProp)+","+str(indProp)+ \
 						","+str(avgP)+","+str(avgQ)+","+str(avgI)+","+str(avgInP)+","+str(avgInQ)+","+str(avgInI)+ \
 						","+str(avgOutP)+","+str(avgOutQ)+","+str(avgOutI)+"\n"
-		#print resultstr
-		self.resultFile.write(resultstr)		
+		self.resultFile.write(resultstr)
 		
-		# individualist strategy type counts
+		# individual-entitative strategy type counts
 		props = self.countIndivStrategyTypes(indAgents)		
 		for strat in self.stratTypes:
 			if strat == self.stratTypes[-1]:
@@ -164,7 +173,7 @@ class Stats:
 			else:			
 				self.indivStratCountFile.write(str(props[strat])+",")
 
-		# coll in strategy type counts
+		# group-entitative in-group strategy type counts
 		props = self.countInStrategyTypes(collAgents)		
 		for strat in self.stratTypes:
 			if strat == self.stratTypes[-1]:
@@ -172,7 +181,7 @@ class Stats:
 			else:			
 				self.inStratCountFile.write(str(props[strat])+",")
 
-		# coll out strategy type counts
+		# group-entitative out-group strategy type counts
 		props = self.countOutStrategyTypes(collAgents)		
 		for strat in self.stratTypes:
 			if strat == self.stratTypes[-1]:
@@ -188,7 +197,7 @@ class Stats:
 
 
 	def getCounts(self, agents):
-		""" Gets counts, averag i, and avg in and out-group strategy p and q in a relationsMatrix"""
+		""" Get counts of agents of each type of tag """
 
 		counts = {}
 		for tag in self.tags:
@@ -200,6 +209,8 @@ class Stats:
 		return counts 
 	
 	def getInPandQCounts(self, agents):
+		""" Counts of p, q and i for group-entitative in-group """
+
 		countP = 0
 		countQ = 0
 		countI = 0
@@ -211,6 +222,8 @@ class Stats:
 		return countP, countQ, countI
 
 	def getOutPandQCounts(self, agents):
+		""" Counts of p, q and i for group-entitative out-group """
+
 		countP = 0
 		countQ = 0
 		countI = 0
@@ -222,6 +235,8 @@ class Stats:
 		return countP, countQ, countI
 	
 	def getPQICounts(self, agents):
+		""" Counts of p, q and i for individual-entitative """
+		
 		countP = 0
 		countQ = 0
 		countI = 0
@@ -284,8 +299,9 @@ class Stats:
 
 		return proportions
 
-
 	def close_files(self):
+		""" close output files """
+
 		self.resultFile.close()
 		self.indivStratCountFile.close()
 		self.inStratCountFile.close()
