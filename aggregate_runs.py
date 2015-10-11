53b413d4742720640bbfd327d6367c7fd0813aa6
Soham De <sohamde@umiacs.umd.edu>
2015-10-11 19:24:47 -0400

    cleaning up plotting and aggregating code

--- aggregate_runs.py
+++ aggregate_runs.py
@@ -1,51 +1,66 @@
-## Aggregates different runs in results folder using settings defined in mp, runs, network_init
+## Averages over different runs in results folder using settings defined in mp, runs, network_init
+##
+## Creates a file *_avg.txt for each set of runs with file names *_{run_number}.txt
+##
 ## authors: soham de, patrick roos
 ## emails: (sohamde, roos) at cs umd edu
 
 import os
 
+# different mobilities. creates a different avg file for each mobility value
 mp = [x/100.0 for x in range(0, 9)]
+
+# number of runs. files names go from *_1.txt to *_{runs}.txt
 runs = 50
 
+# initialization of network used. check 'grid_initialization' parameter in globals.py for a list of different values this parameter can take.
 network_init = 4
+
 # set path to folder where output files will be saved; cluster = 1 if running on DeepThought2 cluster, 0 otherwise
 if os.path.isdir("/lustre/sohamde/Entitativity"):
 	path = '/lustre/sohamde/Entitativity/src/results/'
 else:
 	path = './results/'
 
-# example_file = 'coeff_PD_b0.03c0.01_g4_i1_m0.1_mr50_numneighs4_pairallneighs_1.txt'
+# set prefixes of files
 prefixes = ['coeff', 'inStratCounts', 'indivStratCounts', 'outStratCounts', 'results', 'tagCounts', 'alive', 'diff_games']
+
+# concatenating path with prefixes
 path_prefix = [path+i for i in prefixes]
+
+# beginning and ending strings of the results files
 beginning_str = '_PD_b0.03c0.01_g4_i1_m'
 ending_str = '_mr50_numneighs4_init'+str(network_init)+'_pairallneighs_'
 
+# for each mobility value
 for mp_i in mp:
-	# Creating run ID
+
+	# create run ID
 	run_ID = beginning_str + str(mp_i) + ending_str
 
-	# Nested list for storing values and headers
+	# nested list for storing values and headers
 	values = list()
 	headers = list()
 
-	# Looping over runs
+	# looping over runs
 	first_run = 1
 	for run in range(1,runs+1):
+
 		# Creating file name
 		input_file_names = [i+run_ID+str(run)+".txt" for i in path_prefix]
 
-		# Opening and saving files
+		# Opening each kind of results file
 		for type in range(len(prefixes)):
 
 			if first_run == 1:
 				values.append(list())
 
+			# Opening results files and updating values list
 			f_in = open(input_file_names[type], "rb")
-
 			first_row = 1
-
 			row_count = 0
 			for r in f_in:
+
 				# save values
 				value_str = r.rstrip()
 
@@ -69,15 +84,16 @@ for mp_i in mp:
 
 		first_run = 0
 
-	# saving files
+	# saving average file for this mobility setting
 	output_file_names = [i+run_ID+"avg.txt" for i in path_prefix]
 	for type in range(len(prefixes)):
 		f_out = open(output_file_names[type], "wb")
 		f_out.write(headers[type]+"\n")
 		for l in values[type]:
+			# averaging over runs for each row
 			l_avg = [i/runs for i in l]
 			l_str = [str(i) for i in l_avg]
 			f_out.write(",".join(l_str)+"\n")
 		f_out.close()
 
-	print mp_i
+	print 'completed averaging over files with mobility:', mp_i
